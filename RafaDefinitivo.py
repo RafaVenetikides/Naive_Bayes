b5c242a2e94186be9914aff890d31e064e3d88a4
maturidadejpg <rafavenetikides@gmail.com>
2023-04-25 18:11:03 -0300

    tentativa 1 do predict

--- RafaDefinitivo.py
+++ RafaDefinitivo.py
@@ -4,91 +4,134 @@ from sklearn.datasets import load_breast_cancer, load_digits
 import math
 import collections
 from tabulate import tabulate
+import random
 
-X, y = load_digits(return_X_y=True)
 
 # transferindo valores do load para variaveis para depois transformar em uma funcao
 
-data = X
-labels = y
+def trainNB(data, labels):
+    # num_linhas = quantidade de instancias
+    # num_colunas = quantidade deatributos por instancia
 
-# num_linhas = quantidade de instancias
-# num_colunas = quantidade deatributos por instancia
+    num_linhas = len(data)
+    num_colunas = len(data[0])
 
-num_linhas = len(data)
-num_colunas = len(data[0])
+    # Colocando todos os possiveis resultados em uma matriz
+    numLabels = []
+    for i in labels:
+        if i not in numLabels:
+            numLabels.append(i)
 
-# Colocando todos os possiveis resultados em uma matriz
-numLabels = []
-for i in labels:
-    if i not in numLabels:
-        numLabels.append(i)
+    numLabels.sort()
 
-numLabels.sort()
+    # criando um vetor onde o indice no numLabels eh o mesmo da probabilidade
 
-# criando um vetor onde o indice no numLabels eh o mesmo da probabilidade
+    pLabels = [0] * len(numLabels)
 
-pLabels = [0] * len(numLabels)
+    i = 0
+    j = 0
 
-i = 0
-j = 0
+    # dando outro nome para a variavel para utilizar mais tarde
+    totalLabels = num_linhas
+    somaLabels = []
 
-#dando outro nome para a variavel para utilizar mais tarde
-totalLabels = num_linhas
+    # calculando a probabilidade por resultado e guardando no vetor
+    for i in range(len(numLabels)):
+        sum = 0
+        for j in labels:
+            if j == numLabels[i]:
+                sum += 1
+        somaLabels.append(sum);
+        pLabels[i] = sum / totalLabels
 
-# calculando a probabilidade por resultado e guardando no vetor
-for i in range(len(numLabels)):
-    sum = 0
-    for j in labels:
-        if j == numLabels[i]:
-            sum += 1
-    pLabels[i] = sum/totalLabels
+    # salvando todos os possiveis valores para os atributos em um vetor
 
-#salvando todos os possiveis valores para os atributos em um vetor
+    numData = []
 
-numData = []
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            if data[i][j] not in numData:
+                numData.append(data[i][j])
 
-for i in range(len(data)):
-    for j in range(len(data[0])):
-        if data[i][j] not in numData:
-            numData.append(data[i][j])
+    numData.sort()
 
+    # criando uma matriz "matrizSoma" em que as dimensoes representam: (resultados X numero de atributos X numero de valores para os atributos)
 
-numData.sort()
+    matrizSoma = np.zeros((len(numLabels), num_colunas, len(numData)), dtype=float)
+    pData = np.zeros((num_colunas, len(numData), len(numLabels)), dtype=float)
 
-# criando uma matriz "matrizSoma" em que as dimensoes representam: (resultados X numero de atributos X numero de valores para os atributos)
+    print(f"linhas: {len(pData)}, colunas: {len(pData[0])},profundidade: {len(pData[0][0])}")
 
-matrizSoma = np.zeros((len(numLabels), num_colunas, len(numData)), dtype=float)
-pData = np.zeros((num_colunas, len(numData), len(numLabels)), dtype=float)
+    print(numData)
+    print(numLabels)
 
-print(f"linhas: {len(pData)}, colunas: {len(pData[0])},profundidade: {len(pData[0][0])}")
+    database = np.hstack((data, labels.reshape(-1, 1)))
 
-print(numData)
-print(numLabels)
+    print(f'database linhas: {len(database)}, database colunas: {len(database[0])}')
 
-database = np.hstack((data, labels.reshape(-1, 1)))
+    # Realizando a soma dos valores por posicao, tipo de tom e resultado
 
-print(f'database linhas: {len(database)}, database colunas: {len(database[0])}')
+    for label in range(len(numLabels)):
+        for instance in range(num_linhas):
+            for attribute in range(num_colunas):
+                attribute_value = data[instance][attribute]
+                value_index = numData.index(attribute_value)
+                if labels[instance] == numLabels[label]:
+                    matrizSoma[label][attribute][value_index] += 1
 
-# Realizando a soma dos valores por posicao, tipo de tom e resultado
-
-"""for label in range(len(numLabels)):
-    vetsoma = [[0] * len(numData) for n in range(num_colunas)]
-    for databaseColuna in range(len(database[label]) -1):
-        for databaseLinha in range(len(database)):
-            for tom in range(len(numData)):
-                if database[databaseLinha][-1] == numLabels[label]:
-                    vetsoma[databaseColuna][tom] += 1
-        matrizSoma[label] = vetsoma"""
-
-for label in range(len(numLabels)):
-    for instance in range(num_linhas):
+    for label in range(len(numLabels)):
         for attribute in range(num_colunas):
-            attribute_value = data[instance][attribute]
-            value_index = numData.index(attribute_value)
-            if labels[instance] == numLabels[label]:
-                matrizSoma[label][attribute][value_index] += 1
+            for value_index in range(len(numData)):
+                attribute_label_count = matrizSoma[label][attribute][value_index]
+                label_count = somaLabels[label]
+                pData[attribute][value_index][label] = attribute_label_count / label_count
 
-print(matrizSoma)
+    return pLabels, pData, numLabels, numData
 
-# MATRIZ GERADA CORRETAMENTE!!!
+
+def predictNB(pLabels, pData, instance, numLabels, numData):
+    # num_linhas = quantidade de atributos por instancia
+    num_linhas = len(instance)
+
+    # vetor que guardara as probabilidades para cada resultado
+    pResults = [0] * len(numLabels)
+
+    # calculando a probabilidade para cada resultado
+    for result in range(len(numLabels)):
+        # probabilidade inicial eh a probabilidade do resultado em si
+        p = pLabels[result]
+
+        # para cada atributo, calcula a probabilidade do valor do atributo dado o resultado
+        for attribute in range(num_linhas):
+            attribute_value = instance[attribute]
+            value_index = np.where(numData == attribute_value)[0][0]
+            p *= pData[attribute][value_index][result]
+
+        # adiciona a probabilidade do resultado na lista de probabilidades
+        pResults[result] = p
+
+    # retorna o resultado com maior probabilidade
+    return numLabels[pResults.index(max(pResults))]
+
+
+X, y = load_digits(return_X_y=True)
+
+pLabels, pData, numLabels, numData = trainNB(X, y)
+
+instance = [[0 for j in range(8)] for i in range(8)]
+
+# Preencher a matriz com valores aleatórios entre 0 e 16
+for i in range(8):
+    for j in range(8):
+        instance[i][j] = random.randint(0, 16)
+
+# Imprimir a matriz
+for i in range(8):
+    for j in range(8):
+        print(instance[i][j], end=' ')
+    print()
+
+result = predictNB(pLabels, pData, instance, numLabels, numData)
+
+print(result)
+
